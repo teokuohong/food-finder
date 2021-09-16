8ac5678f78f4984b297b7228cce334d8e88332ce
teokuohong <teokuohongg@gmail.com>
2021-09-16 14:39:40 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import colors from "tailwindcss/colors"
+
+export const purge = [];
+export const darkMode = false;
+export const theme = {
+  extend: {},
+};
+export const variants = {
+  extend: {},
+};
+export const plugins = [];
